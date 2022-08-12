1eab6d82d841d6c3748c6613d3aea20bc28b79c9
Giuseppe <peastuti@gmail.com>
2022-08-12 18:24:31 +0200

    fixes

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Strapi plugin label-manager
 
 A quick description of label-manager.
+
+## Requirements
+Create a new collection type named `Label` (multi-locale) with two fields
+- `key` (Text) and disable the localization for this field
+- `value` (Long Text)
+
+## Install the plugin 
+Inside your project folder, execute
+
+```
+# yarn
+yarn add strapi-plugin-label-manager
+
+# npm
+npm install strapi-plugin-label-manager
+```
--- admin/src/api/labels.js
+++ admin/src/api/labels.js
@@ -2,33 +2,33 @@ import { request } from "@strapi/helper-plugin";
 
 const labelsRequests = {
   getAllLocales: async () => {
-    return await request("/label-manager/locales", {
+    return await request("/strapi-plugin-label-manager/locales", {
       method: "GET",
     });
   },
   
   getAllLabels: async () => {
-    return await request("/label-manager/find", {
+    return await request("/strapi-plugin-label-manager/find", {
       method: "GET",
     });
   },
 
   addLabel: async (data) => {
-    return await request(`/label-manager/create`, {
+    return await request(`/strapi-plugin-label-manager/create`, {
       method: "POST",
       body: data,
     });
   },
 
   editLabel: async (id, value, newValue) => {
-    return await request(`/label-manager/update/${id}`, {
+    return await request(`/strapi-plugin-label-manager/update/${id}`, {
       method: "PUT",
       body: { locale: value.locale, id: value.id, newValue: newValue },
     });
   },
 
   deleteLabel: async (id) => {
-    return await request(`/label-manager/delete/${id}`, {
+    return await request(`/strapi-plugin-label-manager/delete/${id}`, {
       method: "DELETE",
     });
   },
